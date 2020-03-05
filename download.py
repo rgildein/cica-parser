9f9bd79e7a8f4dc74718e6e3d2e779e231bdea74
Robert Gildein <gildeinrobert@gmail.com>
2020-03-05 09:05:36 +0100

    add name verify

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from typing import Tuple
+from typing import Tuple, List
 
 import click
 from selenium import webdriver
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from utils.data import save_data
 from utils.logger import set_up_logger
 from utils.parser import cica, get_cadastral_areas, get_districts, get_letters, get_owner_list, get_surnames
+from utils.translate import remove_slovak_alphabet
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +25,12 @@ def get_owners(
     save_data(output_file, data)  # save data
 
 
+def verify_name(name: str, validated_names: List[str]) -> bool:
+    """verify that the name is listed in the names list"""
+    clean_name = remove_slovak_alphabet(name.lower())
+    return any([clean_name.find(s) > 0 for s in validated_names])
+
+
 @click.command()
 @click.option("--surnames", "-s", multiple=True, type=click.STRING, required=True)
 @click.option("--debug", is_flag=True, default=False, help="logging debug mode")
@@ -34,6 +41,7 @@ def download(surnames: Tuple[str], debug: bool, console: bool):
 
     set_up_logger(debug, console, log_file=f"{output}.log")  # set up logger
     letters = list(set([surname[0].upper() for surname in surnames]))
+    surnames = list(set([remove_slovak_alphabet(surname.lower()) for surname in surnames]))
     logger.info(f"find all names starting with: {letters}")
     logger.info(f"find all names: {surnames}")
 
@@ -43,7 +51,7 @@ def download(surnames: Tuple[str], debug: bool, console: bool):
                 for letter in tqdm(get_letters(driver, district, area), desc="letter", leave=False):
                     if letter in letters:
                         for surname in tqdm(get_surnames(driver, district, area, letter), desc="surname", leave=False):
-                            if surname in surnames:
+                            if verify_name(surname, surnames):
                                 get_owners(f"{output}.csv", driver, district, area, letter, surname)
                             else:
                                 logger.debug(f"omit the surname {surname}")
